c5292006e4b44f34af73c1d27289f8313f2105d5
Franklin Honigberg <73505616+TheStressTest@users.noreply.github.com>
2026-05-03 21:40:36 -0700

    Add second text label
    - Formatted the file
    - Added second text label
    - Added padding to text label

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -6,36 +6,41 @@
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
 
 //==============================================================================
-GuiProjectAudioProcessorEditor::GuiProjectAudioProcessorEditor (GuiProjectAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
-{
+GuiProjectAudioProcessorEditor::GuiProjectAudioProcessorEditor(
+    GuiProjectAudioProcessor &p)
+    : AudioProcessorEditor(&p), audioProcessor(p) {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize(400, 300);
 }
 
-GuiProjectAudioProcessorEditor::~GuiProjectAudioProcessorEditor()
-{
-}
+GuiProjectAudioProcessorEditor::~GuiProjectAudioProcessorEditor() {}
 
 //==============================================================================
-void GuiProjectAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+void GuiProjectAudioProcessorEditor::paint(juce::Graphics &g) {
+    // (Our component is opaque, so we must completely fill the background with
+    // a solid colour)
+    g.fillAll(
+        getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello Franklin!", getLocalBounds(), juce::Justification::centred, 1);
-    
+    g.setColour(juce::Colours::white);
+    g.setFont(juce::FontOptions(15.0f));
+    /* First argument is the text, second argument is the area that dictates the
+     bounds with which to draw the text And the third argument is the
+     justification */
+    g.drawFittedText("Hello Franklin!", getLocalBounds(),
+                     juce::Justification::centred, 1);
+    // Here, I'm using .expanded to add padding to the text so it's not so
+    // cramped up against the bottom
+    g.drawFittedText("Hello Franklin 2!", getLocalBounds().expanded(0, -20),
+                     juce::Justification::centredBottom, 1);
 }
 
-void GuiProjectAudioProcessorEditor::resized()
-{
+void GuiProjectAudioProcessorEditor::resized() {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 }
